32624ccf9627e8cd79566ccbd05168664e9509e6
Sacchidanandutech <64830706+Sacchidanandutech@users.noreply.github.com>
2020-05-23 16:04:35 +0530

    Add files via upload

---
+++ Utech Website/src/app/_models/user.ts
@@ -0,0 +1,12 @@
+﻿export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    phone: number;
+    dob: number;
+    email: string;
+    pan: string;
+    token: string;
+}
